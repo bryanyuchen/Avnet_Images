3dad9f65fb03cd029d0cf38480cad982b4869a8f
bryanyuchen <Bryan.Chen@Avnet.com>
2017-05-17 11:15:50 -0700

    reducing size

--- SmarthomeSherpaWidget.js
+++ SmarthomeSherpaWidget.js
@@ -10,7 +10,6 @@ var valueStyle = freeboard.getStyleString("values");
         var self = this;
 
         var thisGaugeID = "gauge-" + gaugeID++;
-        var titleElement = $('<h2 class="section-title"></h2>');
         var gaugeElement = $('<div class="gauge-widget" id="' + thisGaugeID + '"></div>');
 	var imageElement = $('<img src = "https://c1.staticflickr.com/5/4159/33782256364_a0a64b798b.jpg" id="my_image" style="width:100%">');
         var gaugeObject;
@@ -18,40 +17,19 @@ var valueStyle = freeboard.getStyleString("values");
 
         var currentSettings = settings;
 
-        function createGauge() {
-            if (!rendered) {
-                return;
-            }
-
-            gaugeElement.empty();
-
-            gaugeObject = new JustGage({
-                id: thisGaugeID,
-                value: (_.isUndefined(currentSettings.min_value) ? 0 : currentSettings.min_value),
-                min: (_.isUndefined(currentSettings.min_value) ? 0 : currentSettings.min_value),
-                max: (_.isUndefined(currentSettings.max_value) ? 0 : currentSettings.max_value),
-                label: currentSettings.units,
-                showInnerShadow: false,
-                valueFontColor: "#d3d4d4"
-            });
-        }
-
         this.render = function (element) {
             rendered = true;
-            $(element).append(imageElement).append(titleElement).append($('<div class="gauge-widget-wrapper"></div>').append(gaugeElement));
-            createGauge();
+            $(element).append(imageElement).append($('<div class="gauge-widget-wrapper"></div>').append(gaugeElement));
         }
 
         this.onSettingsChanged = function (newSettings) {
             if (newSettings.min_value != currentSettings.min_value || newSettings.max_value != currentSettings.max_value || newSettings.units != currentSettings.units) {
                 currentSettings = newSettings;
-                createGauge();
             }
             else {
                 currentSettings = newSettings;
             }
 
-            titleElement.html(newSettings.title);
         }
 
         this.onCalculatedValueChanged = function (settingName, newValue) {
@@ -64,46 +42,25 @@ var valueStyle = freeboard.getStyleString("values");
         }
 
         this.getHeight = function () {
-            return 4;
+            return 10;
         }
 
         this.onSettingsChanged(settings);
     };
 
     freeboard.loadWidgetPlugin({
-        type_name: "gauge",
-        display_name: "Gauge",
+        type_name: "smarthomeSherpa",
+        display_name: "SmarthomeSherpa Widget",
         "external_scripts" : [
             "plugins/thirdparty/raphael.2.1.0.min.js",
             "plugins/thirdparty/justgage.1.0.1.js"
         ],
         settings: [
-            {
-                name: "smarthomeSherpa",
-                display_name: "SmarthomeSherpa Widget",
-                type: "text"
-            },
+
             {
                 name: "value",
                 display_name: "Value",
                 type: "calculated"
-            },
-            {
-                name: "units",
-                display_name: "Units",
-                type: "text"
-            },
-            {
-                name: "min_value",
-                display_name: "Minimum",
-                type: "text",
-                default_value: 0
-            },
-            {
-                name: "max_value",
-                display_name: "Maximum",
-                type: "text",
-                default_value: 100
             }
         ],
         newInstance: function (settings, newInstanceCallback) {
