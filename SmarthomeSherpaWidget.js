8ebf29f5112b95330ac69e9885963d6f2a8db7e2
bryanyuchen <Bryan.Chen@Avnet.com>
2017-05-17 12:31:20 -0700

    chane

--- SmarthomeSherpaWidget.js
+++ SmarthomeSherpaWidget.js
@@ -2,20 +2,19 @@
 {
 var valueStyle = freeboard.getStyleString("values");
 
-	freeboard.addStyle('.gauge-widget-wrapper', "width: 100%;text-align: center;");
-	freeboard.addStyle('.gauge-widget', "width:200px;height:160px;display:inline-block;");
+	freeboard.addStyle('.image-widget-wrapper', "width: 100%;text-align: center;");
 
     var SSWidget = function (settings) {
         var self = this;
 
-        //var gaugeElement = $('<div class="gauge-widget" id="' + thisGaugeID + '"></div>');
+        var imageElement = $('<div class="image-widget-wrapper" ></div>');
 		var imageElementTop = $('<img src = "https://c1.staticflickr.com/5/4159/33782256364_a0a64b798b.jpg" id="my_image" style="width:100%">');
 		var imageElementBot = $('<img src = "https://c1.staticflickr.com/5/4158/33781839124_3a8cfff44b_z.jpg" id="my_image" style="width:100%">');
 
         var currentSettings = settings;
 
         this.render = function (element) {
-            $(element).append(imageElementTop, imageElementBot);
+            $(element).append(imageElement).append(imageElementTop, imageElementBot);
         }
 
         this.onSettingsChanged = function (newSettings) {
@@ -44,7 +43,6 @@ var valueStyle = freeboard.getStyleString("values");
             "plugins/thirdparty/justgage.1.0.1.js"
         ],
         settings: [
-
             {
                 name: "value",
                 display_name: "Datasource",
