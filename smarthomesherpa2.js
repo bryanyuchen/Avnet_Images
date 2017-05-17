e7057e321d1cf343ccec9ba97f826fbd188739c3
bryanyuchen <bryanyuchen@gmail.com>
2017-05-17 11:17:03 -0700

    awedsfasd

--- smarthomesherpa2.js
+++ smarthomesherpa2.js
@@ -2,198 +2,65 @@
 {
 var valueStyle = freeboard.getStyleString("values");
 
-	freeboard.addStyle('.widget-big-text', valueStyle + "font-size:75px;");
-
-	freeboard.addStyle('.tw-display', 'width: 100%; height:100%; display:table; table-layout:fixed;');
-
-	freeboard.addStyle('.tw-tr','display:table-row;');
-
-	freeboard.addStyle('.tw-tg','display:table-row-group;');
-
-	freeboard.addStyle('.tw-tc','display:table-caption;');
-
-	freeboard.addStyle('.tw-td','display:table-cell;');
-
-	freeboard.addStyle('.tw-value',	valueStyle +'overflow: hidden;' +'display: inline-block;' +'text-overflow: ellipsis;');
-
-	freeboard.addStyle('.tw-unit','display: inline-block;' +'padding-left: 10px;' +	'padding-bottom: 1.1em;' +	'vertical-align: bottom;');
-	
-	freeboard.addStyle('.tw-value-wrapper',	'position: relative;' +	'vertical-align: middle;' +	'height:100%;');
-
-	freeboard.addStyle('.tw-sparkline',	'height:20px;');
+   var gaugeID = 0;
+	freeboard.addStyle('.gauge-widget-wrapper', "width: 100%;text-align: center;");
+	freeboard.addStyle('.gauge-widget', "width:200px;height:160px;display:inline-block;");
 
     var SSWidget = function (settings) {
-
         var self = this;
 
-        var currentSettings = settings;
-		var displayElement = $('<div class="tw-display"></div>');
-		var titleElement = $('<h2 class="section-title tw-title tw-td"></h2>');
-		var imageElement = $('<img src = "https://c1.staticflickr.com/5/4159/33782256364_a0a64b798b.jpg" id="my_image" style="width:100%">');
-        var valueElement = $('<div class="tw-value"></div>');
-        var unitsElement = $('<div class="tw-unit"></div>');
-        var sparklineElement = $('<div class="tw-sparkline tw-td"></div>');
+        var thisGaugeID = "gauge-" + gaugeID++;
+        var gaugeElement = $('<div class="gauge-widget" id="' + thisGaugeID + '"></div>');
+	var imageElement = $('<img src = "https://c1.staticflickr.com/5/4159/33782256364_a0a64b798b.jpg" id="my_image" style="width:100%">');
+        var gaugeObject;
+        var rendered = false;
 
-		function updateValueSizing()
-		{
-			if(!_.isUndefined(currentSettings.units) && currentSettings.units != "") // If we're displaying our units
-			{
-				valueElement.css("max-width", (displayElement.innerWidth() - unitsElement.outerWidth(true)) + "px");
-			}
-			else
-			{
-				valueElement.css("max-width", "100%");
-			}
-		}
+        var currentSettings = settings;
 
         this.render = function (element) {
-			$(element).empty();
-
-			$(displayElement)
-				.append($('<div class="tw-tr"></div>').append(titleElement))
-				.append($('<div class="tw-tr"></div>').append($('<div class="tw-value-wrapper tw-td"></div>').append(valueElement).append(imageElement).append(unitsElement)))
-				.append($('<div class="tw-tr"></div>').append(sparklineElement));
-
-			$(element).append(displayElement);
-
-			updateValueSizing();
+            rendered = true;
+            $(element).append(imageElement).append($('<div class="gauge-widget-wrapper"></div>').append(gaugeElement));
         }
 
         this.onSettingsChanged = function (newSettings) {
-            currentSettings = newSettings;
+            if (newSettings.min_value != currentSettings.min_value || newSettings.max_value != currentSettings.max_value || newSettings.units != currentSettings.units) {
+                currentSettings = newSettings;
+            }
+            else {
+                currentSettings = newSettings;
+            }
 
-			var shouldDisplayTitle = (!_.isUndefined(newSettings.title) && newSettings.title != "");
-			var shouldDisplayUnits = (!_.isUndefined(newSettings.units) && newSettings.units != "");
-
-			if(newSettings.sparkline)
-			{
-				sparklineElement.attr("style", null);
-			}
-			else
-			{
-				delete sparklineElement.data().values;
-				sparklineElement.empty();
-				sparklineElement.hide();
-			}
-
-			if(shouldDisplayTitle)
-			{
-				titleElement.html((_.isUndefined(newSettings.title) ? "" : newSettings.title));
-				titleElement.attr("style", null);
-			}
-			else
-			{
-				titleElement.empty();
-				titleElement.hide();
-			}
-
-			if(shouldDisplayUnits)
-			{
-				unitsElement.html((_.isUndefined(newSettings.units) ? "" : newSettings.units));
-				unitsElement.attr("style", null);
-			}
-			else
-			{
-				unitsElement.empty();
-				unitsElement.hide();
-			}
-
-			var valueFontSize = 30;
-
-			if(newSettings.size == "big")
-			{
-				valueFontSize = 75;
-
-				if(newSettings.sparkline)
-				{
-					valueFontSize = 60;
-				}
-			}
-
-			valueElement.css({"font-size" : valueFontSize + "px"});
-
-			updateValueSizing();
         }
 
-		this.onSizeChanged = function()
-		{
-			updateValueSizing();
-		}
-
         this.onCalculatedValueChanged = function (settingName, newValue) {
-            if (settingName == "value") {
-
-                if (currentSettings.animate) {
-                    easeTransitionText(newValue, valueElement, 500);
-                }
-                else {
-                    valueElement.text(newValue);
-                }
-
-                if (currentSettings.sparkline) {
-                    addValueToSparkline(sparklineElement, newValue);
-                }
+            if (!_.isUndefined(gaugeObject)) {
+                gaugeObject.refresh(Number(newValue));
             }
         }
 
         this.onDispose = function () {
-
         }
 
         this.getHeight = function () {
-            return 4;
+            return 10;
         }
 
         this.onSettingsChanged(settings);
     };
 
     freeboard.loadWidgetPlugin({
-        type_name: "SmarthomeSherpa",
-        display_name: "SmarthomeSherpa",
+        type_name: "smarthomeSherpa",
+        display_name: "SmarthomeSherpa Widget",
         "external_scripts" : [
-            "plugins/thirdparty/jquery.sparkline.min.js"
+            "plugins/thirdparty/raphael.2.1.0.min.js",
+            "plugins/thirdparty/justgage.1.0.1.js"
         ],
         settings: [
-            {
-                name: "title",
-                display_name: "Title",
-                type: "text"
-            },
-            {
-                name: "size",
-                display_name: "Size",
-                type: "option",
-                options: [
-                    {
-                        name: "Regular",
-                        value: "regular"
-                    },
-                    {
-                        name: "Big",
-                        value: "big"
-                    }
-                ]
-            },
+
             {
                 name: "value",
                 display_name: "Value",
                 type: "calculated"
-            },
-            {
-                name: "sparkline",
-                display_name: "Include Sparkline",
-                type: "boolean"
-            },
-            {
-                name: "animate",
-                display_name: "Animate Value Changes",
-                type: "boolean",
-                default_value: true
-            },
-            {
-                name: "units",
-                display_name: "Units",
-                type: "text"
             }
         ],
         newInstance: function (settings, newInstanceCallback) {
