826f50c391f8f94d18235951f9925dcbf561095f
bryanyuchen <Bryan.Chen@Avnet.com>
2017-05-17 09:14:39 -0700

    ss widget

---
+++ Freeboard Plugins/SmarthomeSherpaWidget.js
@@ -0,0 +1,107 @@
+(function()
+{
+	freeboard.addStyle('div.pointer-value', "position:absolute;height:95px;margin: auto;top: 0px;bottom: 0px;width: 100%;text-align:center;");
+    var SmarthomeSherpa = function (settings) {
+        var self = this;
+        var paper;
+        var strokeWidth = 3;
+        var triangle;
+        var width, height;
+        var currentValue = 0;
+        var valueDiv = $('<p style="font-size:62px; color: rgb(212,212,212); position:relative; left:-10px; top: -50px; align: center;"><img src = 'https://c1.staticflickr.com/5/4158/33781839124_3a8cfff44b_z.jpg' style='width:100%'></p>');
+        var unitsDiv = $('<div style="position:relative; top:-100px; left:-10px; "></div>');
+
+        function polygonPath(points) {
+            if (!points || points.length < 2)
+                return [];
+            var path = []; //will use path object type
+            path.push(['m', points[0], points[1]]);
+            for (var i = 2; i < points.length; i += 2) {
+                path.push(['l', points[i], points[i + 1]]);
+            }
+            path.push(['z']);
+            return path;
+        }
+
+        this.render = function (element) {
+            width = $(element).width();
+            height = this.getHeight()*60;//$(element).height();
+
+            var radius = Math.min(width, height) / 2 - strokeWidth * 2;
+
+            paper = Raphael($(element).get()[0], width, height);
+            var circle = paper.circle(width / 2, height / 2, radius);
+            circle.attr("stroke", "#FF9900");
+            circle.attr("stroke-width", strokeWidth);
+
+            triangle = paper.path(polygonPath([width / 2, (height / 2) - radius + strokeWidth, 15, 20, -30, 0]));
+            triangle.attr("stroke-width", 0);
+            triangle.attr("fill", "#fff");
+
+            $(element).append($('<div class="pointer-value"><img src = "https://raw.githubusercontent.com/bryanyuchen/Avnet_Images/master/sstoponnew.gif" style="width:100%"></div>').append(valueDiv).append(unitsDiv));
+        }
+
+        this.onSettingsChanged = function (newSettings) {
+            unitsDiv.html(newSettings.units);
+        }
+
+        this.onCalculatedValueChanged = function (settingName, newValue) {
+            if (settingName == "direction") {
+                if (!_.isUndefined(triangle)) {
+                    var direction = "r";
+
+                    var oppositeCurrent = currentValue + 180;
+
+                    if (oppositeCurrent < newValue) {
+                        //direction = "l";
+                    }
+
+                    triangle.animate({transform: "r" + newValue + "," + (width / 2) + "," + (height / 2)}, 250, "bounce");
+                }
+
+                currentValue = newValue;
+            }
+            else if (settingName == "value_text") {
+                valueDiv.html(newValue);
+            }
+        }
+
+        this.onDispose = function () {
+        }
+
+        this.getHeight = function () {
+            return 4;
+        }
+
+        this.onSettingsChanged(settings);
+    };
+
+    freeboard.loadWidgetPlugin({
+        type_name: "smarthomeSherpa",
+        display_name: "Smarthome Sherpa",
+        "external_scripts" : [
+            "plugins/thirdparty/raphael.2.1.0.min.js"
+        ],
+        settings: [
+            {
+                name: "direction",
+                display_name: "Direction",
+                type: "calculated",
+                description: "In degrees"
+            },
+            {
+                name: "value_text",
+                display_name: "Value Text",
+                type: "calculated"
+            },
+            {
+                name: "units",
+                display_name: "Units",
+                type: "text"
+            }
+        ],
+        newInstance: function (settings, newInstanceCallback) {
+            newInstanceCallback(new SmarthomeSherpa(settings));
+        }
+    });
+}());
