89b6c35260d95d4c4f72996260f913b4468399b1
bryanyuchen <bryanyuchen@gmail.com>
2017-05-17 23:47:23 -0700

    Update smarthomesherpa2.js

--- smarthomesherpa2.js
+++ smarthomesherpa2.js
@@ -1,38 +1,60 @@
 (function()
 {
-var valueStyle = freeboard.getStyleString("values");
-
 	freeboard.addStyle('.image-widget-wrapper', "width: 100%;text-align: center;");
 
     var SSWidget = function (settings) {
         var self = this;
+		
+		//light variables
 		var lightOn = true;
+		var maxLightLevel = 1601; // set this to the output level when you shine a light on the sensor
+		var minLightLevel = 1;   // set this to the ambient light level or close to 0
+		var increment = (maxLightLevel-minLightLevel)/8;
+		var lightPrev = 0;
 		
 		//define images
-		var ssTopOn = $('<img src = "https://raw.githubusercontent.com/bryanyuchen/Avnet_Images/master/sstoponnew.gif" style="width:100%">');
-		var ssTopOff = $('<img src = "https://c1.staticflickr.com/5/4159/33782256364_a0a64b798b.jpg" style="width:100%">');
-		var ssBot0 = $('<img src = "https://c1.staticflickr.com/5/4158/33781839124_3a8cfff44b_z.jpg" style="width:100%">');
-		var ssBot1 = $('<img src = "https://c1.staticflickr.com/5/4187/34238935210_12737c2d95_z.jpg" style="width:100%">');
-		var ssBot2 = $('<img src = "https://c1.staticflickr.com/5/4188/34238935340_e323f1e6d4_z.jpg" style="width:100%">');
-		var ssBot3 = $('<img src = "https://c1.staticflickr.com/5/4160/33781839224_ee21c38988_z.jpg" style="width:100%">');
-		var ssBot4 = $('<img src = "https://c1.staticflickr.com/5/4182/34238935430_cee460e635_z.jpg" style="width:100%">');
-		var ssBot5 = $('<img src = "https://c1.staticflickr.com/5/4160/33781839344_325ddab700_z.jpg" style="width:100%">');
-		var ssBot6 = $('<img src = "https://c1.staticflickr.com/5/4169/34624904525_29c74b198a_z.jpg" style="width:100%">');
-		var ssBot7 = $('<img src = "https://c1.staticflickr.com/5/4188/33781839434_a98c5d6eca_z.jpg" style="width:100%">');
-		var ssBot8 = $('<img src = "https://c1.staticflickr.com/5/4167/34238935020_fcf814c037_z.jpg" style="width:100%">');
+		var ssTopOn = $('<div id="div1" style="display:none; position: absolute;"><img src = "https://raw.githubusercontent.com/bryanyuchen/Avnet_Images/master/sstoponnew.gif" style="width:100%"></div>');
+		var ssTopOff = $('<div id="div2" style="display:none; position: absolute;"><img src = "https://c1.staticflickr.com/5/4159/33782256364_a0a64b798b.jpg" style="width:100%"></div>');
+		var ssBot0 = $('<div id="div3" style="display:none; position: absolute;"><img src = "https://c1.staticflickr.com/5/4158/33781839124_3a8cfff44b_z.jpg" style="width:100%"></div>');
+		var ssBot1 = $('<div id="div4" style="display:none; position: absolute;"><img src = "https://c1.staticflickr.com/5/4187/34238935210_12737c2d95_z.jpg" style="width:100%"></div>');
+		var ssBot2 = $('<div id="div5" style="display:none; position: absolute;"><img src = "https://c1.staticflickr.com/5/4188/34238935340_e323f1e6d4_z.jpg" style="width:100%"></div>');
+		var ssBot3 = $('<div id="div6" style="display:none; position: absolute;"><img src = "https://c1.staticflickr.com/5/4160/33781839224_ee21c38988_z.jpg" style="width:100%"></div>');
+		var ssBot4 = $('<div id="div7" style="display:none; position: absolute;"><img src = "https://c1.staticflickr.com/5/4182/34238935430_cee460e635_z.jpg" style="width:100%"></div>');
+		var ssBot5 = $('<div id="div8" style="display:none; position: absolute;"><img src = "https://c1.staticflickr.com/5/4160/33781839344_325ddab700_z.jpg" style="width:100%"></div>');
+		var ssBot6 = $('<div id="div9" style="display:none; position: absolute;"><img src = "https://c1.staticflickr.com/5/4169/34624904525_29c74b198a_z.jpg" style="width:100%"></div>');
+		var ssBot7 = $('<div id="div10" style="display:none; position: absolute;"><img src = "https://c1.staticflickr.com/5/4188/33781839434_a98c5d6eca_z.jpg" style="width:100%"></div>');
+		var ssBot8 = $('<div id="div11" style="display:block; position: relative;"><img src = "https://c1.staticflickr.com/5/4167/34238935020_fcf814c037_z.jpg" style="width:100%"></div>');
 		
 		//define states
         var imageElement = $('<h6 class="image-widget-wrapper" style="background-color: rgb(65,195,99);"></h6>');
 		var imageElementTop = ssTopOff;
-		var imageElementBreak = $('<br>');
-		var imageElementBot = ssBot8;
-		var imageElementBotPrev;
+		var imageElementBreak = $('<br style="display:none;">');
 		
         var currentSettings = settings;
-
+	
+		function refreshLight() {
+			$(ssBot0).css("display","none");
+			$(ssBot0).css("position","absolute");
+			$(ssBot1).css("display","none");
+			$(ssBot1).css("position","absolute");
+			$(ssBot2).css("display","none");
+			$(ssBot2).css("position","absolute");
+			$(ssBot3).css("display","none");
+			$(ssBot3).css("position","absolute");
+			$(ssBot4).css("display","none");
+			$(ssBot4).css("position","absolute");
+			$(ssBot5).css("display","none");
+			$(ssBot5).css("position","absolute");
+			$(ssBot6).css("display","none");
+			$(ssBot6).css("position","absolute");
+			$(ssBot7).css("display","none");
+			$(ssBot7).css("position","absolute");
+			$(ssBot8).css("display","none");
+			$(ssBot8).css("position","absolute");
+		}
         this.render = function (element) {
 			$(element).empty();
-			$(imageElement).append(imageElementTop, imageElementBreak, imageElementBot);
+			$(imageElement).append(ssTopOn, ssTopOff, imageElementBreak, ssBot0,ssBot1,ssBot2,ssBot3,ssBot4,ssBot5,ssBot6,ssBot7,ssBot8);
             $(element).append(imageElement);
         }
 
@@ -42,10 +64,70 @@ var valueStyle = freeboard.getStyleString("values");
 
         this.onCalculatedValueChanged = function (settingName, newValue) {
 			if (settingName == "fanData"){
-				alert(newValue);
+				if (newValue == 4){
+					$(ssTopOn).css("display","block");
+					$(ssTopOn).css("position","relative");
+					$(ssTopOff).css("display","none");
+					$(ssTopOff).css("position","absolute");
+				}
+				else {
+					$(ssTopOff).css("display","block");
+					$(ssTopOff).css("position","relative");
+					$(ssTopOn).css("display","none");
+					$(ssTopOn).css("position","absolute");
+				}	
 			}
 			if (settingName == "lightData"){
-				alert(newValue);
+				
+				if (newValue != lightPrev) {
+					alert(newValue);
+					refreshLight();
+					if (newValue == 0){
+						$(ssBot0).css("display","block");
+						$(ssBot0).css("position","relative");
+					}
+					else if (newValue < (1*increment))
+					{
+						$(ssBot1).css("display","block");
+						$(ssBot1).css("position","relative");
+					}
+					else if (newValue < (2*increment))
+					{
+						$(ssBot2).css("display","block");
+						$(ssBot2).css("position","relative");
+					}
+					else if (newValue < (3*increment))
+					{
+						$(ssBot3).css("display","block");
+						$(ssBot3).css("position","relative");
+					}
+					else if (newValue < (4*increment))
+					{
+						$(ssBot4).css("display","block");
+						$(ssBot4).css("position","relative");
+					}
+					else if (newValue < (5*increment))
+					{
+						$(ssBot5).css("display","block");
+						$(ssBot5).css("position","relative");
+					}
+					else if (newValue < (6*increment))
+					{
+						$(ssBot6).css("display","block");
+						$(ssBot6).css("position","relative");
+					}
+					else if (newValue < (7*increment))
+					{
+						$(ssBot7).css("display","block");
+						$(ssBot7).css("position","relative");
+					}
+					else {
+						$(ssBot8).css("display","block");
+						$(ssBot8).css("position","relative");
+					}
+					
+					lightPrev = newValue;
+				}
 			}
         }
 
@@ -62,10 +144,6 @@ var valueStyle = freeboard.getStyleString("values");
     freeboard.loadWidgetPlugin({
         type_name: "smarthomeSherpa",
         display_name: "SmarthomeSherpa Widget",
-        "external_scripts" : [
-            "plugins/thirdparty/raphael.2.1.0.min.js",
-            "plugins/thirdparty/justgage.1.0.1.js"
-        ],
         settings: [
             {
                 name: "fanData",
