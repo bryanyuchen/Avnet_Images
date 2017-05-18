f710acb73f0f22fb7eb90ff4f511c350021f49c0
bryanyuchen <Bryan.Chen@Avnet.com>
2017-05-17 22:46:08 -0700

    try fdan

--- SmarthomeSherpaWidget.js
+++ SmarthomeSherpaWidget.js
@@ -9,17 +9,17 @@
 		var lightOn = true;
 		var lightPrev = 0;
 		//define images
-		var ssTopOn = $('<img src = "https://raw.githubusercontent.com/bryanyuchen/Avnet_Images/master/sstoponnew.gif" style="width:100%">');
-		var ssTopOff = $('<img src = "https://c1.staticflickr.com/5/4159/33782256364_a0a64b798b.jpg" style="width:100%">');
-		var ssBot0 = $('<img src = "https://c1.staticflickr.com/5/4158/33781839124_3a8cfff44b_z.jpg" style="width:100%">');
-		var ssBot1 = $('<img src = "https://c1.staticflickr.com/5/4187/34238935210_12737c2d95_z.jpg" style="width:100%">');
-		var ssBot2 = $('<img src = "https://c1.staticflickr.com/5/4188/34238935340_e323f1e6d4_z.jpg" style="width:100%">');
-		var ssBot3 = $('<img src = "https://c1.staticflickr.com/5/4160/33781839224_ee21c38988_z.jpg" style="width:100%">');
-		var ssBot4 = $('<img src = "https://c1.staticflickr.com/5/4182/34238935430_cee460e635_z.jpg" style="width:100%">');
-		var ssBot5 = $('<img src = "https://c1.staticflickr.com/5/4160/33781839344_325ddab700_z.jpg" style="width:100%">');
-		var ssBot6 = $('<img src = "https://c1.staticflickr.com/5/4169/34624904525_29c74b198a_z.jpg" style="width:100%">');
-		var ssBot7 = $('<img src = "https://c1.staticflickr.com/5/4188/33781839434_a98c5d6eca_z.jpg" style="width:100%">');
-		var ssBot8 = $('<img src = "https://c1.staticflickr.com/5/4167/34238935020_fcf814c037_z.jpg" style="width:100%">');
+		var ssTopOn = $('<div id="div1" style="display:none; position: absolute;"><img src = "https://raw.githubusercontent.com/bryanyuchen/Avnet_Images/master/sstoponnew.gif" style="width:100%"></div>');
+		var ssTopOff = $('<div id="div2" style="display:none; position: absolute;"><img src = "https://c1.staticflickr.com/5/4159/33782256364_a0a64b798b.jpg" style="width:100%"></div>');
+		var ssBot0 = $('<div id="div3" style="display:none; position: absolute;"><img src = "https://c1.staticflickr.com/5/4158/33781839124_3a8cfff44b_z.jpg" style="width:100%"></div>');
+		var ssBot1 = $('<div id="div4" style="display:none; position: absolute;"><img src = "https://c1.staticflickr.com/5/4187/34238935210_12737c2d95_z.jpg" style="width:100%"></div>');
+		var ssBot2 = $('<div id="div5" style="display:none; position: absolute;"><img src = "https://c1.staticflickr.com/5/4188/34238935340_e323f1e6d4_z.jpg" style="width:100%"></div>');
+		var ssBot3 = $('<div id="div6" style="display:none; position: absolute;"><img src = "https://c1.staticflickr.com/5/4160/33781839224_ee21c38988_z.jpg" style="width:100%"></div>');
+		var ssBot4 = $('<div id="div7" style="display:none; position: absolute;"><img src = "https://c1.staticflickr.com/5/4182/34238935430_cee460e635_z.jpg" style="width:100%"></div>');
+		var ssBot5 = $('<div id="div8" style="display:none; position: absolute;"><img src = "https://c1.staticflickr.com/5/4160/33781839344_325ddab700_z.jpg" style="width:100%"></div>');
+		var ssBot6 = $('<div id="div9" style="display:none; position: absolute;"><img src = "https://c1.staticflickr.com/5/4169/34624904525_29c74b198a_z.jpg" style="width:100%"></div>');
+		var ssBot7 = $('<div id="div10" style="display:none; position: absolute;"><img src = "https://c1.staticflickr.com/5/4188/33781839434_a98c5d6eca_z.jpg" style="width:100%"></div>');
+		var ssBot8 = $('<div id="div11" style="display:none; position: absolute;"><img src = "https://c1.staticflickr.com/5/4167/34238935020_fcf814c037_z.jpg" style="width:100%"></div>');
 		
 		//define states
         var imageElement = $('<h6 class="image-widget-wrapper" style="background-color: rgb(65,195,99);"></h6>');
@@ -29,10 +29,13 @@
 		var imageElementBotPrev;
 		
         var currentSettings = settings;
-
+	
+		function updateImageElement() {
+			
+		}
         this.render = function (element) {
 			$(element).empty();
-			$(imageElement).append(imageElementTop, imageElementBreak, imageElementBot);
+			$(imageElement).append(ssTopOn, ssTopOff, imageElementBreak, imageElementBot);
             $(element).append(imageElement);
         }
 
@@ -42,8 +45,12 @@
 
         this.onCalculatedValueChanged = function (settingName, newValue) {
 			if (settingName == "fanData"){
-				imageElementTop = ssTopOn;
-				this.render();
+				if (newValue == 4){
+				ssTopOn.css("display","block");
+				}
+				else {
+					ssTopOff.css("display","block")
+				}	
 			}
 			if (settingName == "lightData"){
 				if (newValue != lightPrev) {
