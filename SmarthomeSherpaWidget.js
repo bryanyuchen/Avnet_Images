6df8a71c2d114b4d0bc7d92b795e4da2978f924c
bryanyuchen <bryanyuchen@gmail.com>
2017-05-24 11:40:48 -0700

    Update SmarthomeSherpaWidget.js

--- SmarthomeSherpaWidget.js
+++ SmarthomeSherpaWidget.js
@@ -17,26 +17,27 @@
 		//define images
 		var ssTopOn = $('<img src = "https://raw.githubusercontent.com/bryanyuchen/Avnet_Images/master/sstoponnew.gif" style="width:100%; display:none;" >');
 		var ssTopOff = $('<img src = "https://c1.staticflickr.com/5/4159/33782256364_a0a64b798b.jpg" style="width:100%; display:none;" >');
-		var ssBot0 = $('<img src = "https://c1.staticflickr.com/5/4158/33781839124_3a8cfff44b_z.jpg" style="width:100%; opacity:0; top: 360px; z-index:2;" >');
-		var ssBot8 = $('<img src = "https://c1.staticflickr.com/5/4167/34238935020_fcf814c037_z.jpg" style="width:100%; opacity:1; top: 360px; z-index:1;" >');
+		var ssBot0 = $('<img src = "https://c1.staticflickr.com/5/4158/33781839124_3a8cfff44b_z.jpg" style="width:100%; opacity:0; z-index:2;" >');
+		var ssBot8 = $('<img src = "https://c1.staticflickr.com/5/4167/34238935020_fcf814c037_z.jpg" style="width:100%; opacity:1; z-index:1;" >');
 		
-		var txt1 = "<p>Text.</p>";              // Create text with HTML
-		
+	   	 //light on text
+		var lightOnTxt = "<h2 id='lightOnText' style='font-size:210%; position: absolute; top:500px ;left: 36px; width: 100%; color: white; z-index:3;'>Light On!</h2>";
+	    
 		//define states
-        var imageElement = $('<h6 class="image-widget-wrapper" style="background-color: rgb(65,195,99);"></h6>');
+        	//var imageElement = $('<h6 class="image-widget-wrapper" style="background-color: rgb(65,195,99);"></h6>');
 		var imageElementDiv = $('<div id="cf"></div>');
-		var imageElementTopDiv = $('<div id="top" style="top:0"></div>');
-		var imageElementBotDiv = $('<div id="bottom" style="top:300px"></div>');
+		var imageElementTopDiv = $('<div id="top" style="top:0; position:relative;"></div>');
+		var imageElementBotDiv = $('<div id="bottom" style="position:relative;"></div>');
 		
         var currentSettings = settings;
 	
         this.render = function (element) {
 			$(element).empty();
-			$(imageElementDiv).append(imageElementTopDiv, imageElementBotDiv);
+			$(imageElementDiv).append(imageElementTopDiv, imageElementBotDiv, lightOnTxt);
 			$(imageElementTopDiv).append(ssTopOn, ssTopOff);
 			$(imageElementBotDiv).append(ssBot0,ssBot8);
 
-            $(element).append(imageElementDiv);
+           		 $(element).append(imageElementDiv);
         }
 
         this.onSettingsChanged = function (newSettings) {
@@ -50,21 +51,29 @@
 					$(ssTopOn).css("position","relative");
 					$(ssTopOff).css("display","none");
 					$(ssTopOff).css("position","absolute");
+					
 				}
 				else {
 					$(ssTopOff).css("display","block");
 					$(ssTopOff).css("position","relative");
 					$(ssTopOn).css("display","none");
 					$(ssTopOn).css("position","absolute");
+					
 				}	
 			}
 			if (settingName == "lightData"){
-				
 				if (newValue != lightPrev) {
 					//fade in
 					fadeValue = 1 - (newValue / maxLightLevel);
-					$(ssBot0).css("opacity",fadeValue);	
+					$(ssBot0).css("opacity",fadeValue);
+					if (newValue > maxLightLevel) {
+						$("#lightOnText").css("display","none");
+					}
+					else {
+						$("#lightOnText").css("display","initial");
+					}
 				}
+				
 			}
         }
 
